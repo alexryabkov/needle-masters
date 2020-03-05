2f5fc1b5cc717f35ee3f879a7873e7348f6f6b56
Alexander Ryabkov <alexryabkov@gmail.com>
2020-03-05 23:14:11 +0300

    first commit

--- needle_app/config.py
+++ needle_app/config.py
@@ -5,16 +5,15 @@ import os
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(
     app.root_path, 'site_data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SECRET_KEY'] = \
-    b'x\xd9"\x84\xbe\tB}J\xa6DZx\xf6\xf9\x89\x08\xb5P+{\x03\xad\xdf'
+app.config['SECRET_KEY'] = ''
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
-app.config['MAIL_TO'] = ['e.filatova18@mail.ru']
+app.config['MAIL_TO'] = ['test@mail.ru']
 # app.config['MAIL_TO'] = ['ramnn@mail.ru']
 app.config['MAIL_SERVER'] = 'smtp.mail.ru'
 app.config['MAIL_PORT'] = 465
 app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = 'adm.shtuchki@mail.ru'
-app.config['MAIL_PASSWORD'] = '123QWEasd!!!'
+app.config['MAIL_USERNAME'] = 'mysuperadmin@mail.ru'
+app.config['MAIL_PASSWORD'] = 'dummy!'
 
 db = SQLAlchemy(app)
 IMAGES_RELATIVE_PATH = 'images/'
